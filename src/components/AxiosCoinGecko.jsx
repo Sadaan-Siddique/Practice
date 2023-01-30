8bdc3d3a0ef71780f1c245b37e5d6c86191f2f76
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-31 00:15:31 +0500

    Errors aarhe hein

--- src/components/AxiosCoinGecko.jsx
+++ src/components/AxiosCoinGecko.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 function AxiosCoinGecko() {
-   const [ coinData , setCoinData] = useState([]);
-   axios.get('https://api.coingecko.com/api/v3/coins/list').then((res)=>{
-    console.log(res.data[0]);
-   }).catch((err)=>{
+  const [coinData, setCoinData] = useState([]);
+  axios.get('https://api.coingecko.com/api/v3/coins/list').then((res) => {
+    // setCoinData(res.data)
+    console.log(res.data[0].id);
+  }).catch((err) => {
     console.log(err)
-   });
-    return (
-    <div>AxiosCoinGecko</div>
+  });
+  return (
+    <>
+      <div>AxiosCoinGecko</div>
+      <h1>{coinData.data}</h1>
+    </>
   )
 }
 
-export default AxiosCoinGecko
+export default AxiosCoinGecko 
