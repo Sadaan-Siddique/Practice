5a76715b0477dbd20a75cbd409c5338534df375c
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-02-09 15:33:19 +0500

    Post

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { useRef, useState, useEffect } from 'react';
+import axios from 'axios'
 function Contact() {
-// use State
-    const [ num , setNum ] = useState(0);
-    function submitfunc(e){
+    // use State
+    const [num, setNum] = useState(0);
+    // use Ref
+    const inptEmail = useRef();
+    const inptPassword = useRef();
+
+    function submitfunc(e) {
         e.preventDefault();
-        setNum(num + 1)
-        let numb = 0;
-        numb += 1
-        console.log('Clicked'+' '+num+' '+numb);
-        // alert('Submit Kro');
+        console.log(inptEmail.current.value);
+        console.log(inptPassword.current.value);
+        inptEmail.current.value = ' ';
+        inptPassword.current.value = ' ';
+        let url = 'https://comfortable-gold-belt.cyclic.app/signup';
+        axios.post(url, data, { headers }).then((res)=>{
+            console.log(res);
+        }).catch((err)=>{
+            console.log(err);
+        });
     }
     return (
         <>
-            <div className='text-center mt-5'>
-                <label>Email :</label><input className='ps-2 py-1' type="text" />
-                <label>Password :</label><input className='ps-2 py-1' type="text" />
+            <form className='form text-center mt-5 container'>
+                <label>Email :</label><input ref={inptEmail} className='form-control ps-2 py-1' type="text" placeholder='Type Email' />
+                <label>Password :</label><input ref={inptPassword} className='form-control ps-2 py-1' type="text" placeholder='Type Password' />
                 <button type='submit' onClick={submitfunc}>Submit</button>
-            </div>
+            </form>
         </>
     )
 }
