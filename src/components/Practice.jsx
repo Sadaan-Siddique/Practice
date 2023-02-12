d87168053cd4c9e8108b16031e17ede0296b512f
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-02-12 22:19:31 +0500

    tr edit

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -15,7 +15,10 @@ function Practice() {
         }
     }
     const editfunc = () => {
-        
+        // props(`Hello${<>
+        //     <input type="text" />
+        //     <button>Click</button>
+        // </>}`)
     }
     useEffect(() => {
         console.log(inptArr);
@@ -42,13 +45,40 @@ function Practice() {
                                 <>
                                     {/* <li>{item.inptObj.inptData}</li> */}
                                     <tr>
-                                        <td onClick={editfunc}>{item}</td>
+                                        <td className="" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo" style={{ cursor: 'pointer' }} onClick={editfunc}>{item}</td>
                                     </tr>
                                 </>
                             )
                         })}
                     </tbody>
                 </table>
+            </div>
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">New message</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label for="recipient-name" className="col-form-label">Recipient:</label>
+                                    <input type="text" className="form-control" id="recipient-name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="message-text" className="col-form-label">Message:</label>
+                                    <textarea className="form-control" id="message-text"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Send message</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
