0acfa480b6386d01e6582de9a6953ab99eba4dde
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-02-12 19:57:40 +0500

    Border Add kiya hai

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -7,8 +7,15 @@ function Practice() {
     const inptText = useRef();
     // js
     const btnfunc = (e) => {
-        e.preventDefault();
-        setInptData(inptText.current.value)
+        if (inptText.current.value == '') {
+            alert('Type some Value');
+        } else {
+            e.preventDefault();
+            setInptData(inptText.current.value)
+        }
+    }
+    const editfunc = () => {
+        
     }
     useEffect(() => {
         console.log(inptArr);
@@ -18,17 +25,24 @@ function Practice() {
     }, [inptData]);
     return (
         <>
-            <div className='container text-center mt-5'>
-                <input ref={inptText} type="text" className='p-1' />
-                <button onClick={btnfunc} className='btn btn-outline-dark fw-bold'>Click</button>
-                <table className='table'>
+
+            <div className='container w-50 text-center mt-4 ps-5 pe-5'>
+                <h1>Todo List In React  </h1>
+                <input ref={inptText} type="text" className='ps-3 w-100 text-center py-1 mt-3' />
+                <button onClick={btnfunc} className='ms-1 my-2 btn btn-outline-dark fw-bold'>Click</button>
+                <table className='table border'>
+                    <thead>
+                        <tr>
+                            <th className='h3 fw-bold'>List</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         {inptArr.map((item) => {
                             return (
                                 <>
                                     {/* <li>{item.inptObj.inptData}</li> */}
                                     <tr>
-                                        <td>{item}</td>
+                                        <td onClick={editfunc}>{item}</td>
                                     </tr>
                                 </>
                             )
